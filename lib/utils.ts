8f94498eee1c712924c042f02ad477624f0381fe
Rohit <rohitgotey@gmail.com>
2024-07-21 11:59:04 +0530

    generated all data from summary end point

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
+import exp from "constants"
+import { eachDayOfInterval, isSameDay } from "date-fns"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -26,4 +28,37 @@ export function calcultePercentChange(current: number, previous: number) {
     return (previous === current) ? 0 : 100
   }
   return ((current - previous) / previous) * 100
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date,
+    income: Number,
+    expenses: Number
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length == 0) {
+    return []
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate
+  })
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day))
+
+    if (found) {
+      return found;
+    }
+    else return {
+      date: day,
+      income: 0,
+      expenses: 0
+    }
+  })
+
+  return transactionsByDay
 }
